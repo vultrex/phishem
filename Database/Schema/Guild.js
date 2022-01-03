743cdf774130f1e49f5aa4ab6ec90f13cdbf5018
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-01-03 15:39:37 -0800

    Did some mongo shit, now to work on the filters

---
+++ Database/Schema/Guild.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model("Guild", new mongoose.Schema({
+
+    id: { type: String }, //ID of the guild
+    // Logger
+    config: {
+        delete: { type: Boolean, default: true }, //Delete the guild after the bot is restarted
+        youtube_filter: { type: Boolean, default: false }, //Filter youtube links
+        action_ban: { type: Boolean, default: false }, //Ban the user after an action
+        action_kick: { type: Boolean, default: false }, //Kick the user after an action
+        action_timeout: { type: Boolean, default: false }, //Mute the user after an action
+    },
+    log: {
+        webhookID: { type: String, default: null }, //ID of the webhook
+        webhookToken: { type: String, default: null }, //Token of the webhook
+        webhookChannelID: { type: String, default: null }, //ID of the channel the webhook is in
+    }
+
+
+
+}));
--- events/client/guildDelete.js
+++ events/client/guildDelete.js
@@ -1,10 +1,7 @@
+const Schema = require("../../Database/Schema/Guild")
+
 module.exports = async (client, guild) => {
-    client.db.delete(`${guild.id}.config.delete`)
-    client.db.delete(`${guild.id}.config.log.channelId`)
-    client.db.delete(`${guild.id}.config.log.id`)
-    client.db.delete(`${guild.id}.config.log.token`)
-    client.db.delete(`${guild.id}.config.youtube`)
-    client.db.delete(`${guild.id}.config.ban`)
-    client.db.delete(`${guild.id}.config.kick`)
-    client.db.delete(`${guild.id}.config.timeout`)
+    Schema.findOneAndDelete({ id: guild.id }, (err, res) => {
+        if (err) console.log(err)
+    })
 }
