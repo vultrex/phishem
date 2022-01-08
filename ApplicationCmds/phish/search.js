e330ef39b69e1c31eb6dfcbd6b32d964ce4b9a39
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-01-07 19:32:55 -0800

    Search fix

--- ApplicationCmds/phish/search.js
+++ ApplicationCmds/phish/search.js
@@ -16,10 +16,9 @@ module.exports = {
         try {
 
 
-        const regex = new RegExp(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]?/gi);
-       const domain = interaction.options._hoistedOptions[0].value.match(regex)[0]
-        const inf = await client.phish.phisherman(domain)
-            const dns = await client.phish.dnsSearch(domain)
+            const regex = new RegExp(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]?/gi);
+            const domain = interaction.options._hoistedOptions[0].value.match(regex)[0]
+            const inf = await client.phish.phisherman(domain)
             /*
             new MessageEmbed()
                      .setColor('RANDOM')
@@ -35,39 +34,39 @@ module.exports = {
                          iconURL: interaction.member.avatarURL({dynamic: true})
                      })
              */
-         if(!inf[domain] || inf[domain].classification === 'unknown') return interaction.reply({embeds: [ new MessageEmbed().setColor('RED').setDescription(domain).addField("Classification",  "❓ Unknown")]})
-        if(inf[domain].classification === 'safe') return interaction.reply({embeds: [new MessageEmbed().setDescription(domain).setColor("GREEN").addField('Classification', '<:2585modshieldlightgreenicon:927289585761927168> Safe') ]})
-        let embed = new MessageEmbed()
+            if(!inf[domain] || inf[domain].classification === 'unknown') return interaction.reply({embeds: [ new MessageEmbed().setColor('RED').setDescription(domain).addField("Classification",  "❓ Unknown")]})
+            if(inf[domain].classification === 'safe') return interaction.reply({embeds: [new MessageEmbed().setDescription(domain).setColor("GREEN").addField('Classification', '<:2585modshieldlightgreenicon:927289585761927168> Safe') ]})
+            let embed = new MessageEmbed()
 
-        let status = inf[`${domain}`].status
-        if(status === 'ONLINE') {
-            status = "🟢 Online"
-        } else {
-            status = "🔴 Offline"
-        }
+            let status = inf[`${domain}`].status
+            if(status === 'ONLINE') {
+                status = "🟢 Online"
+            } else {
+                status = "🔴 Offline"
+            }
 
-        let verified = inf[`${domain}`].verifiedPhish
-        verified = !!verified
-        if(verified === true) {
-            verified = "<:9294passed:926715199950561341> Verified"
-        } else {
-            verified = "<:3595failed:926715200172867624> Not Verified"
-        }
+            let verified = inf[`${domain}`].verifiedPhish
+            verified = !!verified
+            if(verified === true) {
+                verified = "<:9294passed:926715199950561341> Verified"
+            } else {
+                verified = "<:3595failed:926715200172867624> Not Verified"
+            }
 
-        let classification = inf[`${domain}`].classification
-        if(classification === 'suspicious') {
-            embed.setColor("YELLOW")
-            classification = "<:2189modshieldyellowicon:929213568535117884> Suspicious"
-        } else if(classification === 'malicious') {
-            embed.setColor('RED')
-            classification = "<:image_20211228_114840:925475350706806875> Malicious"
-        }
+            let classification = inf[`${domain}`].classification
+            if(classification === 'suspicious') {
+                embed.setColor("YELLOW")
+                classification = "<:2189modshieldyellowicon:929213568535117884> Suspicious"
+            } else if(classification === 'malicious') {
+                embed.setColor('RED')
+                classification = "<:image_20211228_114840:925475350706806875> Malicious"
+            }
 
-        let firstSeen = inf[`${domain}`].firstSeen
-        if(!firstSeen) firstSeen = Date.now()
+            let firstSeen = inf[`${domain}`].firstSeen
+            if(!firstSeen) firstSeen = Date.now()
 
-        let lastSeen = inf[`${domain}`].lastSeen
-        if(!lastSeen) lastSeen = Date.now()
+            let lastSeen = inf[`${domain}`].lastSeen
+            if(!lastSeen) lastSeen = Date.now()
             const urlscan = new MessageActionRow()
                 .addComponents(new MessageButton()
                     .setURL(`https://urlscan.io/result/${inf[`${domain}`].details.urlScanId}`)
@@ -75,19 +74,19 @@ module.exports = {
                     .setEmoji('🛠️')
                     .setStyle(5),
                 )
-        embed
-            .setDescription(domain)
-            .addField("__Status__", status, true)
-            .addField("__Verified__", verified, true)
-            .addField("__Classification__", classification, true)
-            .addField("__Added__", `<t:${moment(inf[`${domain}`].created).format("X")}:F>`, true)
-            .addField("__First Seen__", `<t:${moment(firstSeen).format("X")}:F>`, true)
-            .addField("__Last Seen__", `<t:${moment(lastSeen).format("X")}:F>`, true)
-            .addField('__Domain IP__', `${inf[`${domain}`].details.ip_address ? inf[`${domain}`].details.ip_address : 'IP address not found.'}`, true)
-            .addField('__Asn Name__', inf[`${domain}`].details.asn.asn_name ? inf[`${domain}`].details.asn.asn_name : 'No asn name found.', true)
-            .setImage(inf[`${domain}`].details.websiteScreenshot)
+            embed
+                .setDescription(domain)
+                .addField("__Status__", status, true)
+                .addField("__Verified__", verified, true)
+                .addField("__Classification__", classification, true)
+                .addField("__Added__", `<t:${moment(inf[`${domain}`].created).format("X")}:F>`, true)
+                .addField("__First Seen__", `<t:${moment(firstSeen).format("X")}:F>`, true)
+                .addField("__Last Seen__", `<t:${moment(lastSeen).format("X")}:F>`, true)
+                .addField('__Domain IP__', `${inf[`${domain}`].details.ip_address ? inf[`${domain}`].details.ip_address : 'IP address not found.'}`, true)
+                .addField('__Asn Name__', inf[`${domain}`].details.asn.asn_name ? inf[`${domain}`].details.asn.asn_name : 'No asn name found.', true)
+                .setImage(inf[`${domain}`].details.websiteScreenshot)
 
-        interaction.reply({embeds: [embed], components: [urlscan]})
+            interaction.reply({embeds: [embed], components: [urlscan]})
 
         } catch(e) {
             console.log(e)
