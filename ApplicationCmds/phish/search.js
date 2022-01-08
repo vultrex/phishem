a235756e1a221b846a80f028266a7df8623c27d9
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-01-07 19:43:57 -0800

    Search fix

--- ApplicationCmds/phish/search.js
+++ ApplicationCmds/phish/search.js
@@ -36,7 +36,7 @@ module.exports = {
                          iconURL: interaction.member.avatarURL({dynamic: true})
                      })
              */
-            console.log(dns)
+
             if(!inf[domain] || inf[domain].classification === 'unknown') return interaction.reply({embeds: [ new MessageEmbed().setColor('RED').setDescription(domain).addField("Classification",  "❓ Unknown").addField("__Whois Server__", dns.whois_server ? dns.whois_server : "No dns server found", true)
                     .addField("__Registrar__", dns.registrar.name ? dns.registrar.name : "No registrar name found.", true)
                     .addField("__Iana ID__", dns.registrar.iana_id ? dns.registrar.iana_id : "No iana ID found.", true)
@@ -47,7 +47,16 @@ module.exports = {
                         text: `Last updated: ${moment(dns.update_date).format("LL")}`,
                         iconURL: interaction.member.avatarURL({dynamic: true})
                     })]})
-            if(inf[domain].classification === 'safe') return interaction.reply({embeds: [new MessageEmbed().setDescription(domain).setColor("GREEN").addField('Classification', '<:2585modshieldlightgreenicon:927289585761927168> Safe') ]})
+            if(inf[domain].classification === 'safe') return interaction.reply({embeds: [new MessageEmbed().setDescription(domain).setColor("GREEN").addField('Classification', '<:2585modshieldlightgreenicon:927289585761927168> Safe').addField("__Whois Server__", dns.whois_server ? dns.whois_server : "No dns server found", true)
+                    .addField("__Registrar__", dns.registrar.name ? dns.registrar.name : "No registrar name found.", true)
+                    .addField("__Iana ID__", dns.registrar.iana_id ? dns.registrar.iana_id : "No iana ID found.", true)
+                    .addField("__Registrant Name__", dns.registrant.name ? dns.registrant.name : "No name found.", true)
+                    .addField("__Registrant Organization__", dns.registrant.organization ? dns.registrant.organization : "No organization registered.", true)
+                    .addField("__Registrant City__", dns.registrant.city ? dns.registrant.city : "No city registered.", true)
+                    .setFooter({
+                        text: `Last updated: ${moment(dns.update_date).format("LL")}`,
+                        iconURL: interaction.member.avatarURL({dynamic: true})
+                    }) ]})
             let embed = new MessageEmbed()
 
             let status = inf[`${domain}`].status
